99cdfb31f5427892953e0c7c1777ac4b3a8daaf6
Rahul Saini <rahul.saini@velotio.com>
2022-08-03 01:18:21 +0530

    CRUD operations working

---
+++ index.ts
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize';
+import express, { Application, Request, Response } from "express";
+import db from "./models/index";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+//routes
+import todoRoute from "./routes/todo";
+
+const app = express();
+const port = 3000 || process.env.PORT;
+
+// config middleware
+app.use(express.json());
+app.use("/", todoRoute);
+
+// config DB
+db.sequelize.sync()
+  .then(() => {
+    app.listen(port, () => console.log(`server is runing at port ${port}`));
+  })
+  .catch((err: Error) => {
+    console.log(err);
+  });
